01cd0999e0b829b47d89b4a80452f78c31f4f201
Corey Dai <73034147+CDplayerxOne@users.noreply.github.com>
2023-10-22 16:29:37 -0400

    join-leave

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,98 @@
+// Importing packages
 import express from "express";
 import { createServer } from "node:http";
 import { Server, Socket } from "socket.io";
 
+// Initializing Express server
 const app = express();
+
+// Believe this server facilitates the socket.io connections
 const server = createServer(app);
 
+type List = {
+  [key: string]: {
+    roomId: number;
+    members: string[];
+  };
+};
+
+type Turn = {
+  [key: number]: {
+    member: string;
+    index: number;
+  };
+};
+
+// List of all members in each room
+let list: List = {};
+// Keeping track of who's turn it is
+let turn: Turn = {};
+
+let questions = {};
+
+// Initializing Socket.io
 const io = new Server(server, {
+  // To prevent CORS errors
   cors: {
     origin: "http://localhost:8574",
   },
 });
 
+// When a new user hops on
 io.on("connection", (socket: Socket) => {
-  console.log(socket.id);
-  socket.on("joined", (roomId) => {
+  // Client sends 'joined' message to server. roomId is included in the message
+  socket.on("joined", async (roomId) => {
+    // When 'joined' message is received
+    // Join the right room.
     console.log(roomId);
     socket.join(roomId);
+    // Store the room in which the socket is in inside the data property.
+    socket.data.roomId = roomId;
+
+    // If the room list doesn't exist, create it
+    const allSockets = await io.in(roomId).fetchSockets();
+    // If room doesn't exist in the list
+    if (!list[roomId]) {
+      let ids: string[] = [];
+      allSockets.forEach((i) => ids.push(i.id));
+      // Making sure roomId exists
+      if (roomId) {
+        // create the room in the list
+        list[roomId] = {
+          roomId,
+          members: ids,
+        };
+      }
+    } else {
+      // otherwise just update room members
+      let ids: string[] = [];
+      allSockets.forEach((i) => ids.push(i.id));
+      list[roomId] = {
+        roomId,
+        members: ids,
+      };
+    }
+    console.log(list);
     io.to(roomId).emit("admin", `${socket.id} joined`);
+  });
+  socket.on("disconnecting", async () => {
+    console.log("disconnect");
+    // ! since it is disconnecting, it hasn't technically left yet. For some reason
+    // ! If you add a promise, that'll give enough time for the socket to leave the room
+    // ! After which you can update the list
+    await Promise.resolve();
+    // When disconnecting, update the members in the room.
+    let ids: string[] = [];
+    const allSockets = await io.in(socket.data.roomId).fetchSockets();
+    allSockets.forEach((i) => ids.push(i.id));
+    if (socket.data.roomId) {
+      list[socket.data.roomId] = {
+        roomId: socket.data.roomId,
+        members: ids,
+      };
+    }
+
+    console.log(list, "updated");
   });
 });
 
